45180bef0e15bed99dbd682f3578d84404279051
Birk Schmitz <birk.schmitz@q-railing.com>
2017-10-15 16:12:49 +0200

    c

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,7 @@ $("input[type='text']").keypress(function(event){
 			    }else {
 			    	alert("noResp.");
 			    }
-		    	
+
 			    }//end $.post()
 			);
 		}
@@ -65,7 +65,7 @@ $(document).ajaxComplete(function(event,xhr,options){
 	console.log("xhr: \n" + JSON.stringify(xhr));
     console.log("options: \n" + JSON.stringify(options));
 
-    if ( settings.url === "ajax/test.html" ) {
+    if ( options.url === "ajax/test.html" ) {
     	console.log( "Triggered ajaxComplete handler. The result is " + xhr.responseText );
 	}
 
