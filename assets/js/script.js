3188c03fb745c6f33a81d8805caf0e789e1dd25f
Birk Schmitz <birk.schmitz@q-railing.com>
2017-10-15 15:09:21 +0200

    #modfied f-ajaxSucces, stringify(result) Birk Schmitz <birk.schmitz@q-railing.com>Birk Schmitz

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,14 +47,51 @@ $(document).ajaxSuccess(function(event,xhr,options){
 	returnString += "\nEvent: " + event;
 	returnString += "\nxhr: " + xhr;
 	returnString += "\noptions: " + options;
-	JSON.stringify(returnString);
-    alert(returnString);
+	console.log("event: \n" + JSON.stringify(event));
+	console.log("xhr: \n" + JSON.stringify(xhr));
+    console.log("options: \n" + JSON.stringify(options));
 }); 
 	/*
-
-				
-
-#modified http request
+{
+	"url": "https://prod-33.westeurope.logic.azure.com:443/workflows/bbe461a956894542adcd0127b4ae4274/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-INwU77A5otxVOpenUv_l74BXTkhWp7QciHny70Ky0",
+	"type": "POST",
+	"isLocal": false,
+	"global": true,
+	"processData": true,
+	"async": true,
+	"contentType": "application/x-www-form-urlencoded; charset=UTF-8",
+	"accepts": {
+		"*": "*\/*",
+		"text": "text/plain",
+		"html": "text/html",
+		"xml": "application/xml, text/xml",
+		"json": "application/json, text/javascript",
+		"script": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript"
+	},
+	"contents": {
+		"xml": {},
+		"html": {},
+		"json": {},
+		"script": {}
+	},
+	"responseFields": {
+		"xml": "responseXML",
+		"text": "responseText",
+		"json": "responseJSON"
+	},
+	"converters": {
+		"text html": true
+	},
+	"flatOptions": {
+		"url": true,
+		"context": true
+	},
+	"jsonp": "callback",
+	"data": "id=ele&value=1",
+	"dataTypes": ["text"],
+	"crossDomain": true,
+	"hasContent": true
+}
 
 */
 
