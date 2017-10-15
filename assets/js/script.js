028fa8a779c144407ec3ce43fced7537625a54f5
Birk Schmitz <birk.schmitz@q-railing.com>
2017-10-15 17:56:01 +0200

    Working $.ajax()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 if(jQuery){
+
+var planbirk_web_dataExchangeURL = "https://prod-33.westeurope.logic.azure.com/workflows/bbe461a956894542adcd0127b4ae4274/triggers/request/paths/invoke/bsc_Development?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=kc2xFD1cF50OZ_cjM0gZ8GUofni74jlmfxQR5fhM-54";
+var methodPost ="POST";
+var methodGet ="GET";
+var typeOfContent = "JSON";
 	
 $("ul").on("click","li",function(){	
 	$(this).toggleClass("completed");
@@ -19,61 +24,34 @@ $("input[type='text']").keypress(function(event){
 		if(todoText !== ""){
 			$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
 			$(this).val("");
-			var url = "https://prod-33.westeurope.logic.azure.com:443/workflows/bbe461a956894542adcd0127b4ae4274/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=kc2xFD1cF50OZ_cjM0gZ8GUofni74jlmfxQR5fhM-54";			
-		    var method = "POST";
-		    var inputdata2 = {
+
+		    var newItem = {
 		        id: "ele",
-		        value: "1"
+		        value: $(this).text()
 		    };
- //   var inputData = JSON.parse('{"testingElement": "2"}', (key,value) =>
+ 			//   var inputData = JSON.parse('{"testingElement": "2"}', (key,value) =>
 				// typeof value === 'number'
 				// 	? value * 2 
 				// 	: value
 		  //   	); 
 
-		    //{"name":"binchen"};
-			//JSON.stringify(j); // '{"name":"binchen"}'
-
-
-//Send FormData [[[$( "#testform" ).serialize()]]]
-			// $.post(url, method, inputData, function(data, status){ //Note:jQ001
-			// 	if(data === 'yes') {
-			// 	alert("Yes.\nResponseMessage: \n" + data);
-		 //    		$("h1").toggleClass(".httpError");
-		 //    		$("h1").val("Error occurred!")
-			//     }else {
-			//     	alert("noResp.");
-			//     }
- 
-			//     }//end $.post()
-			// );
-
 			/* SYNTAX: $.ajax({name:value, name:value, ... }) */
-	      $.ajax({url: url, type: method, contentType: "JSON", data: inputdata2,
-		    success: function(result,status,xhr){
-		    //result.response.id/value/respone
-        	//status eq "success"
-        	// xhr 
-        		// .status = 200 
-        		// .responseText = [logicapp body]
-        		// .statusText = "success"
-        		// .status.then(function)
-		    	console.log("ajaxSuccess: " + xhr.status + " " + xhr.statusText);
-		    	console.log(xhr.responseText);
-            		$("h1").text(result.message);
-            	console.log($("h1").text());
-            	console.log("changing h1");
-            	console.log($("h1").text());
-        	},
-        	complete: function(xhr,status){ 
-        		console.log("ajaxComplete");
-        		
-        	},
-        	error: function(xhr,status,error){
-        		alert("ajaxError: " + error);
-        	}
-        });
-	      	
+		      $.ajax({url: planbirk_web_dataExchangeURL, type: methodPost, contentType: typeOfContent, data: newItem,
+			    success: function(result,status,xhr){
+			    	$("h1").text(result.message);
+									    //result.response.id/value/respone
+							        	//status eq "success"
+							        	// xhr 
+							        		// .status = 200 
+							        		// .responseText = [logicapp body]
+							        		// .statusText = "success"
+							        		// .status.then(function)
+			    	console.log("ajaxSuccess: \n => Status: " + xhr.status + " StatusText: " + xhr.statusText + " ResponseText: " + xhr.responseText);
+			    	console.log();
+	        	},
+	        	complete: function(xhr,status){ /*console.log("ajaxComplete"); */},
+	        	error: function(xhr,status,error){ alert("ajaxError: " + error); }
+	        });
 		}
 	}
 });
