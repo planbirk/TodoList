47e008acd0a9a66793b4c864eafcafbc9c5fc3a7
Birk Schmitz <birk.schmitz@q-railing.com>
2017-10-15 14:16:23 +0200

    #add http post to Logic App
    Logic App:
    planbirk-web-dataExchange

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,20 @@ $("ul").on("click","span",function(event){
 	});
 		
 });
-
+ 
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var todoText = $(this).val();
 		if(todoText !== ""){
 			$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
-			$(this).val("");	
+			$(this).val("");
+			var url = "https://prod-33.westeurope.logic.azure.com:443/workflows/bbe461a956894542adcd0127b4ae4274/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-INwU77A5otxVOpenUv_l74BXTkhWp7QciHny70Ky0";
+			var data = {"toDoList": "planbirk-web", { "id" : "1", "value" : "yippi"}};
+			var json = pars
+			$.post( url, function( data , status) {
+			  alert("Data: " + data + "\nStatus: " + status);
+			  $( ".result" ).html( data );
+			});
 
 		}
 		
