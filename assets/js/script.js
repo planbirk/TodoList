7ad231a017d425302edc53a7740cd93687157ec0
Birk Schmitz <birk.schmitz@q-railing.com>
2017-10-15 14:23:49 +0200

    #modified http request

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,10 +21,8 @@ $("input[type='text']").keypress(function(event){
 			$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
 			$(this).val("");
 			var url = "https://prod-33.westeurope.logic.azure.com:443/workflows/bbe461a956894542adcd0127b4ae4274/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-INwU77A5otxVOpenUv_l74BXTkhWp7QciHny70Ky0";
-			var data = {"toDoList": "planbirk-web", { "id" : "1", "value" : "yippi"}};
-			var json = pars
 
-		$.post("https://prod-33.westeurope.logic.azure.com:443/workflows/bbe461a956894542adcd0127b4ae4274/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-INwU77A5otxVOpenUv_l74BXTkhWp7QciHny70Ky0",
+		$.post(url,
 	    {
 	        id: "ele",
 	        value: "1"
