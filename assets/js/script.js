4df608cdec78db1877d115ccc1761a0ad0d94f7b
Birk Schmitz <birk.schmitz@q-railing.com>
2017-10-15 14:22:58 +0200

    #modified http request

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,15 +23,22 @@ $("input[type='text']").keypress(function(event){
 			var url = "https://prod-33.westeurope.logic.azure.com:443/workflows/bbe461a956894542adcd0127b4ae4274/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-INwU77A5otxVOpenUv_l74BXTkhWp7QciHny70Ky0";
 			var data = {"toDoList": "planbirk-web", { "id" : "1", "value" : "yippi"}};
 			var json = pars
-			$.post( url, function( data , status) {
-			  alert("Data: " + data + "\nStatus: " + status);
-			  $( ".result" ).html( data );
-			});
 
-		}
+		$.post("https://prod-33.westeurope.logic.azure.com:443/workflows/bbe461a956894542adcd0127b4ae4274/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-INwU77A5otxVOpenUv_l74BXTkhWp7QciHny70Ky0",
+	    {
+	        id: "ele",
+	        value: "1"
+	    },
+	    function(data, status){
+	        alert("Data: " + data + "\nStatus: " + status);
+	    });
 		
 	}
 });
+//xx
+
+
+
 
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle();
