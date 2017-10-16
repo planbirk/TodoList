e3f213ceaf73f6b771952af2f7f4e8c08968b2e1
Birk Schmitz <planbirk@users.noreply.github.com>
2017-10-16 15:17:19 +0200

    setTimeout "O" <> "o"

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,8 +56,8 @@ $("input[type='text']").keypress(function(event){
 $.getJSON({url: getEntityRecords_bsc_development, data: {crud : "read"}})
 .done(function(data){
 		$.each(data, function(index, o){
-			setTimeOut(function(){
-				$("ul").append("<li data-id='" + o.PrimaryId + "''><span><i class='fa fa-trash'></i></span> " + o.Task + "</li>");//.hide().fadeIn(350);
+			setTimeout(function(){
+				$("ul").append("<li data-id='" + o.PrimaryId + "''><span><i class='fa fa-trash'></i></span> " + o.Task + "</li>");.hide().fadeIn(350);
 			},2000);
 		})
 		//console.log("Total records: " + data.length() + "\n\nJSON.stringify(data): " + JSON.stringify(data));
